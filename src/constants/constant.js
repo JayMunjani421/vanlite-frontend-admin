7e3aab50bc9ef8e8bbe89d3dbc84da4991ec6d3b
Jay <munjanijay421@gmail.com>
2025-04-22 10:52:53 +0530

    Update constant.js

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,3 @@
 // export const BASE_URL  = "http://localhost:8000/api/";
 
-export const BASE_URL  = "http://localhost:3000/api/";
+export const BASE_URL  = "https://vanlite-backend.onrender.com/api/";
